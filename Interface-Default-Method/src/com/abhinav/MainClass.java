dc1010e0f04e650b4f225e88e6ae31ff57dee560
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2022-05-13 12:24:45 +0530

    Update MainClass.java

--- Interface-Default-Method/src/com/abhinav/MainClass.java
+++ Interface-Default-Method/src/com/abhinav/MainClass.java
@@ -5,11 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		Vehicle car = new Car();
 		car.vehicleMethod();	//calling default method
-		car.drive();			//calling abstract method	
+		car.drive();		//calling abstract method	
 		
 		Vehicle bus = new Bus();
 		bus.vehicleMethod();	//calling overridden default method
-		bus.drive();			//calling abstract method
+		bus.drive();		//calling abstract method
 		
 	}
 
